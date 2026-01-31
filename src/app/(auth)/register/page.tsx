956620b7c243731f8946c8d4ba80248c577ed340
Rezwan66 <shaikhrezwan66@gmail.com>
2026-01-31 01:03:04 +0100

    feat(auth): add login and register forms and setup next env

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,36 @@
+import { GalleryVerticalEnd } from 'lucide-react';
+
+import { SignupForm } from '@/components/modules/authentication/register-form';
+import Image from 'next/image';
+import Logo from '@/components/modules/shared/Logo';
+
 export default function RegisterPage() {
   return (
-    <div>
-      <h2>Register page</h2>
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="bg-muted relative hidden lg:block">
+        <Image
+          src="/images/auth.jpg"
+          fill
+          alt="Student studying on a laptop"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          {/* <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Acme Inc.
+          </a> */}
+          <Logo />
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SignupForm />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
