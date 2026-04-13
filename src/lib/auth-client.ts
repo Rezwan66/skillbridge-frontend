2fe8b7fa9799d768bac38e3fe0cc465ff313e1b1
Rezwan66 <shaikhrezwan66@gmail.com>
2026-04-14 00:32:36 +0200

    deploy: add cookie plugin to auth-client for safer cookie imports and setting

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,32 @@
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
-  baseURL: typeof window !== 'undefined' 
-    ? window.location.origin 
-    : process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000',
+  baseURL:
+    typeof window !== 'undefined'
+      ? window.location.origin
+      : process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000',
   fetchOptions: {
     credentials: 'include',
   },
+
+  plugins: [
+    {
+      id: 'next-cookies-request',
+      fetchPlugins: [
+        {
+          id: 'next-cookies-request-plugin',
+          name: 'next-cookies-request-plugin',
+          hooks: {
+            async onRequest(ctx) {
+              if (typeof window === 'undefined') {
+                const { cookies } = await import('next/headers');
+                const headers = await cookies();
+                ctx.headers.set('cookie', headers.toString());
+              }
+            },
+          },
+        },
+      ],
+    },
+  ],
 });
